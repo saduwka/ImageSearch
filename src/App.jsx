0e5d8f43170ba50c68ba3a5ebf383e6d2efbf1a7
saduwka <nurzhan13071996@gmail.com>
2025-05-25 16:10:16 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home/Home";
+
+const queryClient = new QueryClient(
+  {defaultOptions: {
+    staleTime: 5 * 60 * 1000,
+    cacheTime: 10* 60 * 1000,
+    refetchOnWindowFocus: false,
+    retry: 2 
+  }
+})
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+    )
+}
